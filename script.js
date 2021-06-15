7e086881afd5ade8aa2aa835263ab8f1796be361
RaviP14 <ravi.patel14@outlook.com>
2021-06-15 17:26:30 +0100

    Added restart button & player names to display.

--- script.js
+++ script.js
@@ -26,13 +26,29 @@ const gameboard = (() => {
     makeGrid(3, 3)
 
     const startGame = document.querySelector('.startGame');
+    const resetGame = document.querySelector('.reset');
+    resetGame.style.display = 'none'
 
     startGame.addEventListener('click', () => {
         playGame.player1.setMyNickname();
+        nickName1 = playGame.player1.getNickname()
         playGame.player2.setMyNickname();
+        //let display = document.querySelector('.display')
+        display.textContent = nickName1 + '\'s (X\'s) turn'
         grid.style.display = 'grid'
         startGame.style.display = 'none'
+        resetGame.style.display = 'block' //change when styling css
     })
+
+    resetGame.addEventListener('click', () => {
+        divs.forEach((div) => {
+            div.textContent = ''
+            arrGameboard = []
+            display.textContent = nickName1 + '\'s (X\'s) turn'
+        })
+    })
+    
+
 
     const divs = document.querySelectorAll('.gridDiv');
     let display = document.querySelector('.display');
@@ -40,18 +56,18 @@ const gameboard = (() => {
     function addPlayerToArray() {
         if (arrGameboard.length === 0) {
             playGame.player1.playerPoint()
-            display.textContent = 'O\'s turn'
+            display.textContent = playGame.player2.getNickname() + '\'s (O\'s) turn'
         } else if (arrGameboard[arrGameboard.length - 1] === 'O') {
             playGame.player1.playerPoint()
-            display.textContent = 'O\'s turn'
+            display.textContent = playGame.player2.getNickname() + '\'s (O\'s) turn'
         } else if (arrGameboard[arrGameboard.length - 1] === 'X') {
             playGame.player2.playerPoint()
-            display.textContent = 'X\'s turn'
+            display.textContent = nickName1 + '\'s (X\'s) turn'
         }
     }
 
     function addSign() {
-        if (this.textContent === '' && (display.textContent === 'X\'s turn' || display.textContent === 'O\'s turn')) {
+        if (this.textContent === '' && (display.textContent === (nickName1 + '\'s (X\'s) turn') || display.textContent === (playGame.player2.getNickname() + '\'s (O\'s) turn'))) {
             addPlayerToArray()
             this.textContent = arrGameboard[arrGameboard.length - 1] //this.value
             playGame.winner()
@@ -76,8 +92,9 @@ const Player = (name, sign) => {
     }
 
     let setMyNickname = () => {
-        let myNickname = prompt( name +'\'s Name?, ')
+        myNickname = prompt( name +'\'s ' + '(' + sign + ')' + ' Name?, ')
         console.log(myNickname)
+        
     }
 
     const getNickname = () => myNickname
@@ -94,7 +111,6 @@ const Player = (name, sign) => {
 const playGame = (() => {
     const player1 = Player('player1', 'X');
     const player2 = Player('player2', 'O');
-
    let div0 = document.querySelector('#div0')
    let div1 = document.querySelector('#div1')
    let div2 = document.querySelector('#div2')
@@ -115,7 +131,7 @@ const playGame = (() => {
         div6.textContent === 'X' && div7.textContent === 'X' && div8.textContent === 'X' ||
         div0.textContent === 'X' && div4.textContent === 'X' && div8.textContent === 'X' ||
         div2.textContent === 'X' && div4.textContent === 'X' && div6.textContent === 'X') {
-            display.textContent = 'X Wins'
+            display.textContent = nickName1 + ' (X) Wins'
         } else if (div0.textContent === 'O' && div3.textContent === 'O' && div6.textContent === 'O' ||
         div1.textContent === 'O' && div4.textContent === 'O' && div7.textContent === 'O' ||
         div2.textContent === 'O' && div5.textContent === 'O' && div8.textContent === 'O' ||
@@ -124,9 +140,9 @@ const playGame = (() => {
         div6.textContent === 'O' && div7.textContent === 'O' && div8.textContent === 'O' ||
         div0.textContent === 'O' && div4.textContent === 'O' && div8.textContent === 'O' ||
         div2.textContent === 'O' && div4.textContent === 'O' && div6.textContent === 'O') {
-            display.textContent = 'O Wins'
-        } else if (div0.textContent !== '' && div1.textContent !== '' && div2.textContent !== '' 
-        && div3.textContent !== '' && div4.textContent !== '' && div5.textContent !== '' &&
+            display.textContent = playGame.player2.getNickname() + ' (O) Wins'
+        } else if (div0.textContent !== '' && div1.textContent !== '' && div2.textContent !== '' && 
+        div3.textContent !== '' && div4.textContent !== '' && div5.textContent !== '' &&
         div6.textContent !== '' && div7.textContent !== '' && div8.textContent !== '') {
             display.textContent = 'It\'s a tie'
         }
